4d2a27c7ad93b313555723f78c7fb593c8e7c325
zhaopeng <719197352@qq.com>
2019-04-19 10:12:34 +0800

    style: fix code style

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { ServerConfig } from '@config/server';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(ServerConfig.port);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(ServerConfig.port);
 
-  Logger.log(`init finished, listen on port ${ServerConfig.port}`, 'bootstrap');
+    Logger.log(
+        `init finished, listen on port ${ServerConfig.port}`,
+        'bootstrap',
+    );
 }
 bootstrap();
