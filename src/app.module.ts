eb74e3daf7b92716f2694e7e08afdb51aa4168bd
zhaopeng <719197352@qq.com>
2019-04-22 11:33:44 +0800

    fix: del error code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common'
 import { DingModule } from './module/ding/ding.module'
-import { DepartmentService } from './module/department/department.service'
 
 @Module({
     imports: [DingModule],
-    providers: [DepartmentService],
 })
 export class AppModule {}
